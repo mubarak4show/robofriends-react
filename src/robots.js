f76d1a9f74385197375198b8ae0de552faaca830
mubarak4show <mubarak4show@yahoo.com>
2018-02-16 10:46:23 +0800

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Yus Robo',
+		username: 'yusrobo',
+		email: 'yusrobo@yahoo.com'
+	},
+	{
+		id: 2,
+		name: 'Tezzy Robo',
+		username: 'tzrobo',
+		email: 'tzrobo@yahoo.com'
+	},
+	{
+		id: 3,
+		name: 'Hiki Robo',
+		username: 'hikirobo',
+		email: 'hikirobo@yahoo.com'
+	},
+	{
+		id: 4,
+		name: 'Loca Robo',
+		username: 'locarobot',
+		email: 'locarobot@gmail.com'
+	},
+	{
+		id: 5,
+		name: 'Doja Robo',
+		username: 'robodoja',
+		email: 'roboddoja@hotmail.com'
+	},
+	{
+		id: 6,
+		name: 'Catia Robo',
+		username: 'catiarobozdos',
+		email: 'catiarobozdos@gmail.com'
+	},
+	{
+		id: 7,
+		name: 'Chika Robo',
+		username: 'chikarodriguez',
+		email: 'chikarodriguez@gmail.com'
+	},
+	{
+		id: 8,
+		name: 'Mamita Bonita',
+		username: 'mabonita',
+		email: 'mabonita@gmail.com'
+	}
+];
